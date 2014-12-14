52b61aa268c4d3e499f282070162b032517395e2
Evan <evturn@gmail.com>
2014-12-14 06:19:42 -0500

    TodoView created to edit, close and update

--- js/views/todos.js
+++ js/views/todos.js
@@ -15,5 +15,23 @@ app.TodoView = Backbone.View.extend({
 		this.$el.html( this.template( this.model.toJSON() ) );
 		this.$input = this.$('.edit');
 		return this;
+	},
+	edit: function() {
+		this.$el.addClass('editing');
+		this.$input.focus();
+	},
+	close: function() {
+		var value = this.$input.val().trim();
+
+		if ( value ) {
+			this.model.save({ title: value });
+		}
+		
+		this.$el.removeClass('editing');
+	},
+	updateOnEnter: function(e) {
+		if ( e.which === ENTER_KEY ) {
+			this.close();
+		}
 	}
 });
