5a0b64fe55e63b5c2d5451dc68b893e1b0f69eee
Evan <evturn@gmail.com>
2014-12-14 11:49:09 -0500

    adding notes for Backbone Todo App

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -3,10 +3,10 @@ var app = app || {};
 var TodoList = Backbone.Collection.extend({
 	model: app.Todo,
 	localStorage: new Backbone.LocalStorage('todos-backbone'),
-    completed: function() {
-    return this.filter(function( todo ) {
-      return todo.get('completed');
-    });
+  completed: function() {
+  return this.filter(function( todo ) {
+    return todo.get('completed');
+  	});
   },
 	remaining: function() {
     return this.without.apply( this, this.completed() );
