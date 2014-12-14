10ab0f5c65ce1cea657f23392f622313813b5bda
Evan <evturn@gmail.com>
2014-12-14 10:53:23 -0500

    app view is fixed

--- js/views/app.js
+++ js/views/app.js
@@ -4,89 +4,89 @@ app.AppView = Backbone.View.extend({
 	el: '#todoapp',
 	statsTemplate: _.template( $('#stats-template').html() ),
 	events: {
-		'keypress #new-todo': 'createOnEnter',
-		'click #clear-completed': 'clearCompleted',
-		'click #toggle-all': 'toggleAllComplete'
-	},
+      'keypress #new-todo': 'createOnEnter',
+      'click #clear-completed': 'clearCompleted',
+      'click #toggle-all': 'toggleAllComplete'
+  },
 	initialize: function() {
-		this.allCheckbox = this.$('#toggle-all')[0];
-		this.$input = this.$('#new-todo');
-		this.$footer = this.$('#footer');
-		this.$main = this.$('#main');
+    this.allCheckbox = this.$('#toggle-all')[0];
+    this.$input = this.$('#new-todo');
+    this.$footer = this.$('#footer');
+    this.$main = this.$('#main');
 
-		this.listenTo(app.Todos, 'add', this.addOne);
-		this.listenTo(app.Todos, 'reset', this.addAll);
-		this.listenTo(app.Todos, 'change:completed', this.filterOne);
-		this.listenTo(app.Todos, 'filter', this.filterAll);
-		this.listenTo(app.Todos, 'all', this.render);
+    this.listenTo(app.Todos, 'add', this.addOne);
+    this.listenTo(app.Todos, 'reset', this.addAll);
 
-		app.Todos.fetch();
-	},
+    this.listenTo(app.Todos, 'change:completed', this.filterOne);
+    this.listenTo(app.Todos,'filter', this.filterAll);
+    this.listenTo(app.Todos, 'all', this.render);
+
+    app.Todos.fetch();
+  },
 	render: function() {
-		var completed = app.Todos.completed().length;
-		var remaining = app.Todos.remaining().length;
+    var completed = app.Todos.completed().length;
+    var remaining = app.Todos.remaining().length;
 
-		if ( app.Todos.length ) {
-			this.$main.show();
-			this.$footer.show();
+    if ( app.Todos.length ) {
+      this.$main.show();
+      this.$footer.show();
 
-			this.$footer.html(this.statsTemplate({
-				completed: completed,
-				remaining: remaining
-			}));
+      this.$footer.html(this.statsTemplate({
+        completed: completed,
+        remaining: remaining
+      }));
 
-			this.$('#filters li a')
-				.removeClass('selected')
-				.filter('[href="#/' + ( app.TodoFilter || '' ) + '"]')
-				.addClass('selected');
-		} else {
-			this.$main.hide();
-			this.$footer.hide();
-		}
+      this.$('#filters li a')
+        .removeClass('selected')
+        .filter('[href="#/' + ( app.TodoFilter || '' ) + '"]')
+        .addClass('selected');
+    } else {
+      this.$main.hide();
+      this.$footer.hide();
+    }
 
-		this.allCheckbox.checked = !remaining;
-	},
+    this.allCheckbox.checked = !remaining;
+  },
 	addOne: function( todo ) {
-		var view = new app.TodoView({ model: todo });
-		$('#todo-list').append( view.render().el );
-	},
+    var view = new app.TodoView({ model: todo });
+    $('#todo-list').append( view.render().el );
+  },
 	addAll: function() {
-		this.$('#todo-list').html('');
-		app.Todos.each(this.addOne, this);
-	},
-	filterOne: function(todo) {
-		todo.trigger('visible');
-	},
-	filterAll: function() {
-		app.Todos.each(this.filterOne, this);
-	},
+      this.$('#todo-list').html('');
+      app.Todos.each(this.addOne, this);
+  },
+	filterOne : function (todo) {
+    todo.trigger('visible');
+  },
+	filterAll : function () {
+      app.Todos.each(this.filterOne, this);
+  },
 	newAttributes: function() {
-		return {
-			title: this.$input.val().trim(),
-			order: app.Todos.nextOrder(),
-			completed: false
-		};
-	},
-	createOnEnter: function( event ) {
-		if ( event.which != ENTER_KEY || this.$input.val().trim() ) {
-			return;
-		}
+    return {
+      title: this.$input.val().trim(),
+      order: app.Todos.nextOrder(),
+      completed: false
+    };
+  },
+  createOnEnter: function( event ) {
+    if ( event.which !== ENTER_KEY || !this.$input.val().trim() ) {
+      return;
+    }
 
-		app.Todos.create( this.newAttributes() );
-		this.$input.val();
-	},
+    app.Todos.create( this.newAttributes() );
+    this.$input.val('');
+  },
 	clearCompleted: function() {
-		_.invoke(app.Todos.completed(), 'destroy');
-		return false;
-	},
+    _.invoke(app.Todos.completed(), 'destroy');
+    return false;
+  },
 	toggleAllComplete: function() {
-		var completed = this.allCheckbox.checked;
+    var completed = this.allCheckbox.checked;
 
-		app.Todos.each(function( todo ) {
-			todo.save({
-				'completed': completed
-			});
-		});
-	}
-
+    app.Todos.each(function( todo ) {
+      todo.save({
+        'completed': completed
+      });
+    });
+  }
 });
